7a112972d34582c24eb1c700d31bcc5a7cee0c7f
ryan <ryanandrewbaxter@gmail.com>
2020-10-20 14:28:41 -0500

    working with hashes

---
+++ store_item.rb
@@ -0,0 +1,6 @@
+apple1 = {:name => "gala", :color => "speckled red", :price => 0.69}
+apple2 = {:name => "fuji", :color => "green/red", :price => 0.69}
+apple3 = {:name => "honeycrisp", :color => "red/yellow", :price => 1.29}
+apple4 = {name: "rave", color: "shiny red/yellow", price: 1.69}
+
+puts "We have so many apples here to choose from! Try #{apple1[:name]}, #{apple2[:name]}, #{apple3[:name]} or even #{apple4[:name]} which is #{apple4[:color]} in color and costs #{apple4[:price]}!"
