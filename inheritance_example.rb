c490925be9f0ccb9e0a820056e8350ddb51ed051
ryan <ryanandrewbaxter@gmail.com>
2020-10-21 15:59:51 -0500

    Classwork from 10/21

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,7 +1,8 @@
 class Vehicle
-  def initialize
-    @speed = 0
-    @direction = 'north'
+  attr_accessor :speed, :direction
+  def initialize(input_options)
+    @speed = input_options[:speed]
+    @direction = input_options[:directon]
   end
 
   def brake
@@ -21,20 +22,39 @@ end
 
 class Bike < Vehicle
 
+  attr_reader :type, :weight, :color
+  def initialize(input_options)
+    super
+    @type = input_options[:type]
+    @weight = input_options[:weight]
+    @color = input_options[:color]
+  end
+
   def ring_bell
     puts "Ring ring!"
   end
+
+  
 end
 
 class Car < Vehicle
+  attr_reader :fuel, :make, :model
+  def initialize(input_options)
+    super
+    @fuel = input_options[:fuel]
+    @make = input_options[:make]
+    @model = input_options[:model]
+  end
+  
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
-car1 = Car.new
+car1 = Car.new(speed: 0, direction: "north", fuel: "Unleaded", make: "Mazda", model: "3")
 car1.accelerate
 puts car1.honk_horn
-bike1 = Bike.new
-bike1.accelerate
-puts bike1.ring_bell
+# bike1 = Bike.new
+# bike1.accelerate
+# puts bike1.ring_bell
+puts car1.direction
